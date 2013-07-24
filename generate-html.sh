9fe64860d0f1c675dc9c92f85ece2b8afc82fcd4
Shane Riddell <shane.riddell@ellucian.com>
2013-07-24 10:33:43 -0400

    Add the HOWTO-iCalendar.md to generate-html

--- generate-html.sh
+++ generate-html.sh
@@ -5,3 +5,4 @@ if [ ! -d target ]
   fi
 # Depenendency:  'color-marked' Node package.  Install with:  'npm install color-marked'
 (cat github-markdown.css.html && color-marked README.md --gfm --color --tables --breaks) > target/README.html
+(cat github-markdown.css.html && color-marked HOWTO-iCalendar.md --gfm --color --tables --breaks) > target/HOWTO-iCalendar.html
