7704f78354dd9e8a8e6ee18bdeabc0e09a0ca030
chasdev <chasdev@me.com>
2014-01-14 16:18:07 -0500

    Remove unused project files; move docs into subdirectory

--- generate-html.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ ! -d target ]
-  then
-    mkdir target
-  fi
-# Depenendency:  'color-marked' Node package.  Install with:  'npm install color-marked'
-(cat github-markdown.css.html && color-marked README.md --gfm --color --tables --breaks) > target/README.html
-(cat github-markdown.css.html && color-marked HOWTO-iCalendar.md --gfm --color --tables --breaks) > target/HOWTO-iCalendar.html
